f7c9b26794c2bc79aba07148edd98feebde5bc0e
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-01 16:23:13 +0000

    updated

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[48:65] + str[0:1] + str[1:6])
-
+str = str[39:67] + 'with ' + str[:6]
+print(str) 
