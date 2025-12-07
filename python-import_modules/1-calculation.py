5acdb37dbd21f4481830e82e3d66e34724bddbbc
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-07 17:34:05 +0000

    commit

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-def add(a, b):
-    return a + b
-def sub(a, b):
-   return a - b
-def mul(a, b):
-    return a * b
-def div(a, b):
-    return int(a / b)
-
 if __name__ == '__main__':
     a = 10
     b = 5
