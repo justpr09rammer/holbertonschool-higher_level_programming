90676fa5d11bed4dbcfd1b56b49fa26f0b0c6c59
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-07 17:18:56 +0000

    task 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    return a + b
+def sub(a, b):
+   return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return int(a / b)
+
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{} * {} = {}'.format(a, b, mul(a, b)))
+    print('{} / {} = {}'.format(a, b, div(a, b)))
