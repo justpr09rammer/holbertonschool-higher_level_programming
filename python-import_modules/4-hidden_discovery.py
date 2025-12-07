cadc28c3dc8caed2c649a7289727d15953bb9937
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-07 18:14:04 +0000

    commit

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,27 +1,9 @@
 #!/usr/bin/python3
-import imp
-import sys
+import hidden_4
 
-def discover_hidden_names():
-    # Load the hidden_4.pyc file from /tmp
-    try:
-        hidden_module = imp.load_compiled("hidden_4", "/tmp/hidden_4.pyc")
-    except FileNotFoundError:
-        print("The file hidden_4.pyc was not found in /tmp.")
-        sys.exit(1)
-    
-    # Get the names defined in the module, excluding those that start with '__'
-    names = dir(hidden_module)
-    filtered_names = [name for name in names if not name.startswith("__")]
-    
-    # Sort the names alphabetically
-    filtered_names.sort()
+if __name__ == '__main__':
+    def_names = dir(hidden_4)
 
-    # Print each name on a new line
-    for name in filtered_names:
-        print(name)
-
-# This ensures the code does not run when the script is imported as a module
-if __name__ == "__main__":
-    discover_hidden_names()
-
+    for i in range(len(def_names)):
+        if def_names[i][:2] != '__':
+            print(def_names[i])
