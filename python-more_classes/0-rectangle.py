f373b50c0ae2d3fac239cb7e24b6adb994ab1d0b
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-14 19:03:46 +0000

    task 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""0-rectangle, built for Holberton project built for machine learning bak ml.
+"""
 
 
 class Rectangle:
-    """Empty class defining a rectangle."""
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
     pass
