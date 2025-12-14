d654d661578546620da95a537ec121d308c94837
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-14 19:58:29 +0000

    commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Module defining the MyList class that inherits from list."""
+"""
+A module to prints a list in ascending order
+"""
+
 
 class MyList(list):
-    """A class that inherits from list and adds a method to print sorted list."""
+    """
+    A class to customize the list class
+    """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order.
+        Prints a list in ascending order
 
-        This method does not modify the original list, it only prints the sorted list.
+        Sort a list and then prints on the output
         """
-        print(sorted(self))
 
+        if issubclass(MyList, list):
+            print(sorted(self))
