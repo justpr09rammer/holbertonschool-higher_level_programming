a7e1a7d7d17aa41ec666b2bc029a467d1a66ca8c
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-14 20:46:45 +0000

    task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,27 +1,28 @@
-"""Python - Input/Output, task 11. Student to JSON"""
-
+#!/usr/bin/python3
+"""Module to define a Student class."""
 
 class Student:
-    """Simple class containing student data.
+    """A class to represent a student."""
 
-    Args:
-        first_name (str): given name of student
-        last_name (str): family name of student
-        age (int): age of student in years
-
-    Attributes:
-        first_name (str): given name of student
-        last_name (str): family name of student
-        age (int): age of student in years
-
-    """
     def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of self.
-
         """
-        return self.__dict__
+        Returns a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the public attributes of the student.
+        """
+        return vars(self)
+
