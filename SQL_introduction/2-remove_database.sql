e34dc486d08e04d5199538c7d969eebddaf618cc
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-21 18:51:17 +0000

    task 2 updated

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Deletes the database `hbtn_0c_0` in MySQL Server.
 --      If the database `hbtn_0c_0` doesn't exists, the script shouldn't fail
-DELETE DATABASE IF EXISTS  hbtn_0c_0; 
+DROP  DATABASE IF EXISTS  hbtn_0c_0; 
